ff4d2ddcc2d439631ec555e44b0649ad0a0b7afe
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2018-08-01 10:14:30 +0200

    add a Cacher interface

--- redis.go
+++ redis.go
@@ -1,6 +1,10 @@
 package redis
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+
 	"github.com/filipovi/redis/config"
 	redis "gopkg.in/redis.v5"
 )
@@ -8,6 +12,56 @@ import (
 // Client is the Redis Client structure
 type Client struct {
 	*redis.Client
+}
+
+// Cacher indicates the cache functions
+type Cacher interface {
+	GetHashKey(data, key string) string
+	Load(key string) (string, error)
+	MultiLoad(keys []string) ([]interface{}, error)
+	Save(key string, data []byte) error
+	FlushAll() error
+}
+
+// GetHashKey returns a key for the data
+func (client Client) GetHashKey(data, key string) string {
+	h := sha256.New()
+	h.Write([]byte(data))
+	return fmt.Sprintf(key, hex.EncodeToString(h.Sum(nil)))
+}
+
+// MultiLoad returns multiple results
+func (client Client) MultiLoad(keys []string) ([]interface{}, error) {
+	data, err := client.MGet(keys...).Result()
+	if nil != err {
+		return nil, err
+	}
+	return data, err
+}
+
+// FlushAll flush the redis database
+func (client Client) FlushAll() error {
+	return client.FlushAll()
+}
+
+// Load the data from Redis
+func (client Client) Load(key string) (string, error) {
+	data, err := client.Get(key).Result()
+	if nil != err {
+		return "", err
+	}
+
+	return data, nil
+}
+
+// Save the data in Redis
+func (client Client) Save(key string, data []byte) error {
+	err := client.Set(key, data, 0).Err()
+	if nil != err {
+		return err
+	}
+
+	return nil
 }
 
 // New returns a Redis Connection
